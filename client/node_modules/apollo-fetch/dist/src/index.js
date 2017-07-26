8ebed0e81f7fa6e02bc3a2481e072484850454ff
ramirobg94 <ramirobg94@gmail.com>
2017-07-26 11:22:13 +0200

    node modules and fist component

---
+++ client/node_modules/apollo-fetch/dist/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var apollo_fetch_1 = require("./apollo-fetch");
+exports.createApolloFetch = apollo_fetch_1.createApolloFetch;
+//# sourceMappingURL=index.js.map
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hands on GraphQL </div>
+    );
+  }
+}
+
+export default App;
